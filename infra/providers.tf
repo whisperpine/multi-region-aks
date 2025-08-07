d853e02d785197f94b125ea57ce470951620d181
Yusong <yusonglai64@gmail.com>
2025-08-07 16:39:27 +0800

    feat: init terraform and add providers

---
+++ infra/providers.tf
@@ -0,0 +1,41 @@
+terraform {
+  # s3 backend docs:
+  # https://developer.hashicorp.com/terraform/language/backend/s3
+  backend "s3" {
+    bucket                      = "tf-states"
+    key                         = "multi-region-aks/terraform.tfstate"
+    endpoints                   = { s3 = "https://00c0277ef0d444bf5c13b03cf8a33405.r2.cloudflarestorage.com" }
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+    use_lockfile                = true
+  }
+
+  # version constraints docs:
+  # https://developer.hashicorp.com/terraform/language/expressions/version-constraints
+  required_version = ">= 1.10"
+  required_providers {
+    sops = {
+      source  = "carlpett/sops"
+      version = "~> 1.2.1"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.38.1"
+    }
+  }
+}
+
+# carlpett/sops provider docs: 
+# https://registry.terraform.io/providers/carlpett/sops/latest/docs
+provider "sops" {}
+
+# hashicorp/azurerm provider docs:
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+provider "azurerm" {
+  features {}
+}
