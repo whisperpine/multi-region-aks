9727dd7438207455c04861329d26db59b9c2a86e
Yusong <yusonglai64@gmail.com>
2025-11-09 17:16:03 +0800

    chore: print the last modified date of "flake.lock"

--- flake.nix
+++ flake.nix
@@ -34,7 +34,11 @@
             ];
             # The shell script executed when the environment is activated.
             shellHook = ''
-              # install git hook managed by husky
+              # Print the last modified date of "flake.lock".
+              stat flake.lock | grep "Modify" |
+                awk '{printf "\"flake.lock\" last modified on: %s", $2}' &&
+                echo " ($((($(date +%s) - $(stat -c %Y flake.lock)) / 86400)) days ago)"
+              # Install git hooks managed by husky.
               if [ ! -e "./.husky/_" ]; then
                 husky install
               fi
